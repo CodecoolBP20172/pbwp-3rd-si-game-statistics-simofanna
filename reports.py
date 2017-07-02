30961c12510e6c95d0ae3a700dd08dcc9a96c634
Simo Fanna <simofanna93@gmail.com>
2017-07-02 23:44:35 +0200

    organization is ready

--- reports.py
+++ reports.py
@@ -1,2 +1,11 @@
+def organization(file_name='game_stat.txt'):
+    game_properties = []
+    file = open(file_name, 'r')
+    all_content = file.read().split('\n')
+    for line in all_content:
+        game = line.split('\t')
+        if len(game) == 5:
+            game_properties.append(game)
+    file.close()
+    return game_properties
 
-# Report functions
