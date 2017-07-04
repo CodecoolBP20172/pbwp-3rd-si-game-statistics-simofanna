4aa2265b643da7e4896c73e752269338756db786
Simo Fanna <simofanna93@gmail.com>
2017-07-04 19:46:19 +0200

    reports organization fix

--- part2/reports.py
+++ part2/reports.py
@@ -5,6 +5,8 @@ def organization(file_name):
     for line in all_content:
         game = line.split('\t')
         if len(game) == 5:
+            game[1] = float(game[1])
+            game[2] = int(game[2])
             game_properties.append(game)
     file.close()
     return game_properties
@@ -12,11 +14,11 @@ def organization(file_name):
 
 def get_most_played(file_name):
     games = organization(file_name)
-    max = 0
+    max_played = 0
     for game_index in range(len(games)):
-        if games[game_index][1] > games[max][1]:
-            max = game_index
-    return games[game_index][0]
+        if games[game_index][1] > games[max_played][1]:
+            max_played = game_index
+    return games[max_played][0]
 
 
 def sum_sold(file_name):
@@ -27,6 +29,22 @@ def sum_sold(file_name):
     sold_sum_list = list(map(float, sold_sum_list))
     total_sold = sum(sold_sum_list)
     return total_sold
+
+
+def get_selling_avg(file_name):
+    return number
+
+
+def count_longest_title(file_name):
+    pass
+
+
+def get_date_avg(file_name):
+    pass
+
+
+def get_game(file_name, title):
+    pass
 
 
 
