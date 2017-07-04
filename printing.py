bea1416d997146335dbe39d0d7dea60ff25c2eb1
Simo Fanna <simofanna93@gmail.com>
2017-07-04 18:15:04 +0200

    printing ready

--- printing.py
+++ printing.py
@@ -1,2 +1,11 @@
-
-# Printing functions
+def print_judy_answers():
+    import reports
+    print(reports.count_games("game_stat.txt"))
+    print(reports.decide("game_stat.txt", "2012"))
+    print(reports.get_latest("game_stat.txt"))
+    print(reports.count_by_genre("game_stat.txt", "Real-time strategy"))
+    print(reports.get_line_number_by_title("game_stat.txt", "Counter-Strike: Condition Zero"))
+    print(reports.sort_abc("game_stat.txt"))
+    print(reports.get_genres("game_stat.txt"))
+    print(reports.when_was_top_sold_fps("game_stat.txt"))
+print_judy_answers()
