325a9cabd8c353af844690a80848b9e093e5fb27
Simo Fanna <simofanna93@gmail.com>
2017-07-04 19:16:23 +0200

    reports.py sum_sold function added

--- part2/reports.py
+++ part2/reports.py
@@ -19,3 +19,14 @@ def get_most_played(file_name):
     return games[game_index][0]
 
 
+def sum_sold(file_name):
+    games = organization(file_name)
+    sold_sum_list = []
+    for game_index in range(len(games)):
+        sold_sum_list.append(games[game_index][1])
+    sold_sum_list = list(map(float, sold_sum_list))
+    total_sold = sum(sold_sum_list)
+    return total_sold
+
+
+
