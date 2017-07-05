fa7c8d4c3d346b2f840d10e11f79b3d88774d217
Simo Fanna <simofanna93@gmail.com>
2017-07-05 02:11:18 +0200

    printing.py added

--- part2/printing.py
+++ part2/printing.py
@@ -1,2 +1,9 @@
-
-# Printing functions
+def print_judy_answers_part2():
+    import reports
+    print(reports.get_most_played("game_stat.txt"))
+    print(reports.sum_sold("game_stat.txt"))
+    print(reports.get_selling_avg("game_stat.txt"))
+    print(reports.count_longest_title("game_stat.txt"))
+    print(reports.get_date_avg("game_stat.txt",))
+    print(reports.get_game("game_stat.txt", 'Minecraft'))
+print_judy_answers_part2()
